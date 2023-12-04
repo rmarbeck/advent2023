6dc3ae521a36c35f867189d02fedb4a69392c0fb
rmarbeck <rafoufou>
2023-12-04 13:56:42 +0100

    cleaner

--- 4-12/src/main/scala/Main.scala
+++ 4-12/src/main/scala/Main.scala
@@ -4,21 +4,19 @@ import scala.math._
 // Right :-/ result is
 
 @main def hello: Unit =
-  println("Launching 1-12-2")
+  println("Launching 4-12")
   val bufferedSource = Source.fromFile("./src/main/resources/test1.txt")
   val lines = bufferedSource.getLines().toSeq
-  val scores = lines.zipWithIndex.map: (line, index) =>
-    line match
-      case s"Card ${number}: ${winnings} | ${ours}" => scorePart1(winnings, ours)
-  val result1 = scores.sum
-  println(s"1 : $result1")
-  val scores2 = lines.map:
-      case s"Card ${number}: ${winnings} | ${ours}" => score(winnings, ours)
-  .zipWithIndex
-  .foldLeft(List.fill(lines.length)(1)): (acc, scoreAndIndex) =>
-    acc.zip(calculateToAddList(acc, scoreAndIndex)).map(_ + _)
-  val result2 = scores2.sum
-  println(s"2 : ${result2}")
+  val (scores1, scores2) = lines.map:
+      case s"Card ${number}: ${winnings} | ${ours}" => (scorePart1(winnings, ours), scorePart2(winnings, ours))
+    .unzip
+
+  println(s"1 : ${scores1.sum}")
+  val result2 = scores2
+    .zipWithIndex
+    .foldLeft(List.fill(lines.length)(1)): (acc, scoreAndIndex) =>
+      acc.zip(calculateToAddList(acc, scoreAndIndex)).map(_ + _)
+  println(s"2 : ${result2.sum}")
   bufferedSource.close
   println("Done")
 
@@ -29,8 +27,12 @@ def calculateToAddList(current: List[Int], scoreAndIndex: (Int, Int)): List[Int]
     innerIndex match
       case value if value > index && value <= (index + score) => valToAdd
       case _ => 0
+
 def scorePart1(winnings: String, ours: String): Int =
   Math.pow(2, score(winnings, ours)-1).toInt
+
+def scorePart2(winnings: String, ours: String): Int =
+  score(winnings, ours)
 
 def score(winnings: String, ours: String): Int =
   val oursCleaned = clean(ours)
