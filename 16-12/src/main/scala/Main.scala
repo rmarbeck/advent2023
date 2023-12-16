ede66831cd30293c65049b8b27fb331bc25113ed
Marbeck <raphael@watchnext.fr>
2023-12-16 18:34:55 +0100

    16-12 best

--- 16-12/src/main/scala/Main.scala
+++ 16-12/src/main/scala/Main.scala
@@ -2,9 +2,6 @@ import com.typesafe.scalalogging.Logger
 import scala.io.Source
 import scala.math._
 
-import scala.collection.parallel.*
-import collection.parallel.CollectionConverters.ImmutableSeqIsParallelizable
-
 import java.time.Duration
 import java.time.Instant
 // Right :-/ result is
@@ -12,6 +9,8 @@ import java.time.Instant
 val loggerAOC = Logger("aoc")
 val loggerAOCPart1 = Logger("aoc.part1")
 val loggerAOCPart2 = Logger("aoc.part2")
+
+import Dir._
 
 @main def hello: Unit =
   loggerAOC.trace("Root trace activated")
@@ -41,7 +40,7 @@ object Solver:
 
     val container1 = Container(lines)
     //println(container1)
-    pathLazyList(List((Beam(Left2Right()), container1.from(Position(0, 0, container1.heigh, container1.width)))), 0).last
+    pathLazyList(List((Beam(LeftToRight), container1.from(Position(0, 0, container1.heigh, container1.width)))), 0).last
     //println("<----------------------->")
     //println(container1)
     //println(container1.calc)
@@ -49,32 +48,26 @@ object Solver:
     val resultOf2 = (for row <- 0 until container1.heigh
         col <- 0 until container1.width
     yield
-      def fromBeam(beam: List[Beam]): Int =
-        beam.map { currentBeam =>
+      def corner(colValue: Int): List[Dir] =
+        colValue match
+          case 0 => LeftToRight :: Nil
+          case value if value == container1.width-1 => RightToLeft :: Nil
+          case _ => Nil
+      val dirs = (row, col) match
+        case (0, valueOfCol) => UpToDown :: corner(valueOfCol)
+        case (value, valueOfCol) if value == container1.heigh-1 => DownToUp :: corner(valueOfCol)
+        case (_, valueOfCol) => corner(valueOfCol)
+      (row, col, dirs)
+      ).map {(row, col, currentDirs) =>
+        currentDirs.map { currentDir =>
           val container = Container(lines)
-          pathLazyList(List((currentBeam, container.from(Position(row, col, container.heigh, container.width)))), 0).lastOption
+          pathLazyList(List((Beam(currentDir), container.from(Position(row, col, container.heigh, container.width)))), 0).lastOption
           container.calc
-        }.max
-      (row, col) match
-        case (0, value) =>
-          value match
-            case 0 => fromBeam(List(Beam(Up2Down()), Beam(Left2Right())))
-            case colValue if colValue == container1.width-1 => fromBeam(List(Beam(Up2Down()), Beam(Right2Left())))
-            case _ => fromBeam(List(Beam(Up2Down())))
-        case (valueRow, valueCol) if valueRow == container1.heigh-1 =>
-          valueCol match
-            case 0 => fromBeam(List(Beam(Down2Up()), Beam(Left2Right())))
-            case colValue if colValue == container1.width-1 => fromBeam(List(Beam(Down2Up()), Beam(Right2Left())))
-            case _ => fromBeam(List(Beam(Down2Up())))
-        case (_, 0) =>
-          fromBeam(List(Beam(Left2Right())))
-        case (_, value) if value == container1.width-1 =>
-          fromBeam(List(Beam(Right2Left())))
-        case _ => 0
-    ).max
+        }.maxOption.getOrElse(0)
+    }.max
 
 
-    val beams = List(Beam(Left2Right()))
+    val beams = List(Beam(LeftToRight))
     var positions = List(Position(0, 0, 10, 10))
 
 
@@ -114,38 +107,21 @@ class Container(input: Seq[String]):
 case class Position(row: Int, col: Int, maxRow: Int, maxCol: Int):
   def next(beam: Beam): Option[Position] =
     beam match
-      case Beam(Up2Down()) => row != maxRow-1 match
+      case Beam(UpToDown) => row != maxRow-1 match
         case true => Some(this.copy(row = row+1))
         case false => None
-      case Beam(Down2Up()) => row != 0 match
+      case Beam(DownToUp) => row != 0 match
         case true => Some(this.copy(row = row - 1))
         case false => None
-      case Beam(Left2Right()) => col != maxCol-1 match
+      case Beam(LeftToRight) => col != maxCol-1 match
         case true => Some(this.copy(col = col + 1))
         case false => None
-      case Beam(Right2Left()) => col != 0 match
+      case Beam(RightToLeft) => col != 0 match
         case true => Some(this.copy(col = col - 1))
         case false => None
 
 
-case class Beam(from: Direction)
-
-sealed trait Direction(val degrees: Int) :
-  def throughSlash: Direction
-  def throughAntiSlash: Direction
-
-case class Up2Down() extends Direction(-90) :
-  override def throughSlash: Direction = Right2Left()
-  override def throughAntiSlash: Direction = Left2Right()
-case class Down2Up() extends Direction(90) :
-  override def throughSlash: Direction = Left2Right()
-  override def throughAntiSlash: Direction = Right2Left()
-case class Left2Right() extends Direction(180) :
-  override def throughSlash: Direction = Down2Up()
-  override def throughAntiSlash: Direction = Up2Down()
-case class Right2Left() extends Direction(0) :
-  override def throughSlash: Direction = Up2Down()
-  override def throughAntiSlash: Direction = Down2Up()
+case class Beam(from: Dir)
 
 class Box(val typology: Char, val positionInContainer: Position, container: Container):
   var lightPassedUpward = false
@@ -155,10 +131,10 @@ class Box(val typology: Char, val positionInContainer: Position, container: Cont
   def passThrough(beam: Beam): List[(Beam, Box)] =
     val result = nextDirections(beam)
     beam.from match
-      case Up2Down() => lightPassedDownward = true
-      case Down2Up() => lightPassedUpward = true
-      case Left2Right() => lightPassedRightward = true
-      case Right2Left() => lightPassedLeftward = true
+      case UpToDown => lightPassedDownward = true
+      case DownToUp => lightPassedUpward = true
+      case LeftToRight => lightPassedRightward = true
+      case RightToLeft => lightPassedLeftward = true
 
     result
   def isPowered: Boolean =
@@ -166,10 +142,10 @@ class Box(val typology: Char, val positionInContainer: Position, container: Cont
 
   def canTranverse(beam: Beam): Boolean =
     beam.from match
-      case Up2Down() if lightPassedDownward => false
-      case Down2Up() if lightPassedUpward => false
-      case Left2Right() if lightPassedRightward => false
-      case Right2Left() if lightPassedLeftward => false
+      case UpToDown if lightPassedDownward => false
+      case DownToUp if lightPassedUpward => false
+      case LeftToRight if lightPassedRightward => false
+      case RightToLeft if lightPassedLeftward => false
       case _ => true
 
   def nextDirections(beam: Beam): List[(Beam, Box)] =
@@ -178,10 +154,10 @@ class Box(val typology: Char, val positionInContainer: Position, container: Cont
       case ('/', value, true) => singleNext(Beam(value.from.throughSlash))
       case ('\\', value, true) => singleNext(Beam(value.from.throughAntiSlash))
       case ('-', value, true) => value.from match
-        case Up2Down() | Down2Up() => List(next(Beam(Left2Right())), next(Beam(Right2Left()))).filterNot(_.isEmpty).map(_.get)
+        case UpToDown | DownToUp => List(next(Beam(LeftToRight)), next(Beam(RightToLeft))).filterNot(_.isEmpty).map(_.get)
         case value => singleNext(Beam(value))
       case ('|', value, true) => value.from match
-        case Left2Right() | Right2Left() => List(next(Beam(Up2Down())), next(Beam(Down2Up()))).filterNot(_.isEmpty).map(_.get)
+        case LeftToRight | RightToLeft => List(next(Beam(UpToDown)), next(Beam(DownToUp))).filterNot(_.isEmpty).map(_.get)
         case value => singleNext(Beam(value))
       case _ => List()
 
@@ -207,3 +183,19 @@ def pathLazyList(beams: List[(Beam, Box)], step: Int): LazyList[Int] =
   }
   if (newBeams.length == 0) LazyList.empty
   else LazyList.cons(step, pathLazyList(newBeams, step + 1))
+
+enum Dir:
+  case UpToDown, DownToUp, LeftToRight, RightToLeft
+  def throughSlash: Dir =
+    this match
+      case UpToDown => RightToLeft
+      case DownToUp => LeftToRight
+      case LeftToRight => DownToUp
+      case RightToLeft => UpToDown
+
+  def throughAntiSlash: Dir =
+    this match
+      case UpToDown => LeftToRight
+      case DownToUp => RightToLeft
+      case LeftToRight => UpToDown
+      case RightToLeft => DownToUp
