f79379e30ef4b6f03fa4f3375f1a23da0afb3203
rmarbeck <rafoufou>
2024-02-09 21:59:24 +0100

    Day 5 - part 2 not working yet

---
+++ day5/src/main/scala/Solution.scala
@@ -0,0 +1,86 @@
+import scala.collection.mutable
+
+object Solution:
+  def run(inputLines: Seq[String]): (String, String) =
+
+    val (rawSeeds, rawMappers) = inputLines.splitAt(2)
+
+    val seeds: Seq[Long] =
+      rawSeeds.head match
+        case s"seeds: $values" => values.split(" ").map(_.toLong).toIndexedSeq
+        case _ => throw Exception("Seeds not recognized")
+
+    val mappers = rawMappers.map:
+      case currentLine if currentLine.isEmpty => "#"
+      case currentLine => currentLine + "/"
+    .mkString
+    .split("#")
+    .map(Mapper.fromString)
+
+    val mappingService = MappingService(mappers)
+
+    val resultPart1 = seeds.map(mappingService.map(_)).min
+
+    val seedsPart2 = seeds.toList.sliding(2, 2).map:
+      case first :: second :: Nil => first until first + second
+
+    val resultPart2 = seedsPart2.map:
+      case currentSeedRange =>
+        currentSeedRange.foldLeft(Long.MaxValue):
+          case (acc, seed) => math.min(acc, mappingService.map(seed))
+    .min
+
+    val result1 = s"$resultPart1"
+    val result2 = s"$resultPart2"
+
+    (s"${result1}", s"${result2}")
+
+end Solution
+
+class MappingService(val mappers: Array[Mapper]):
+  val cachedMap: mutable.Map[Long, Long] = mutable.HashMap[Long, Long]()
+
+  def map(source: Long): Long =
+    cachedMap.getOrElseUpdate(source, doLocate(source))
+
+  private def doLocate(seedValue: Long): Long =
+    mappers.foldLeft(seedValue):
+      case (acc, mapper) => mapper.map(acc)
+
+case class Mapping(destinationStart: Long, sourceStart: Long, length: Long):
+  def matches(source: Long): Boolean =
+    source - sourceStart match
+      case value if value < 0 => false
+      case value if value > length => false
+      case _ => true
+  def follow(source: Long): Long =
+    matches(source) match
+      case false => source
+      case true => destinationStart + source - sourceStart
+
+  def mergeWithPreviousLevel(previousMapping: Mapping): (Option[Mapping], Option[Mapping], Option[Mapping]) = ???
+
+case class Mapper(name: String, mappings: List[Mapping]):
+  def map(source: Long): Long =
+    mappings.find(_.matches(source)) match
+      case Some(mapping) => mapping.follow(source)
+      case None => source
+
+  def mergeWith(other: Mapper): Mapper =
+    def from(mapping: Mapping): List[Mapping] =
+      val (initialDestination, initialSource, initialLength) = (mapping.destinationStart, mapping.sourceStart, mapping.length)
+
+      Nil
+
+    val finalMappings = this.mappings.flatMap(from(_))
+    Mapper(this.name+"-"+other.name, finalMappings)
+
+object Mapper:
+  def fromString(formatedInput: String): Mapper =
+    formatedInput match
+      case s"$name map:/$mappings" =>
+        val foundMappings = mappings.split("/").map:
+          case s"$destination $source $length" => Mapping(destination.toLong, source.toLong, length.toLong)
+          case _ => throw Exception("Not supported format")
+        Mapper(name, foundMappings.toList)
+      case _ => throw Exception("Not supported format")
