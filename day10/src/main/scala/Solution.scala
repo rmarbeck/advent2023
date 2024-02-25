56dae6fea1c56c66463649fb961101c1d4b23884
rmarbeck <rafoufou>
2024-02-25 15:55:27 +0100

    Day 10 init

---
+++ day10/src/main/scala/Solution.scala
@@ -0,0 +1,23 @@
+object Solution:
+  def run(inputLines: Seq[String]): (String, String) =
+
+
+    //
+    // Code is here
+    //
+
+    /**
+     *..F7.
+      .FJ|.
+      SJ.L7
+      |F--J
+      LJ...
+     */
+
+    val result1 = s""
+    val result2 = s""
+
+    (s"${result1}", s"${result2}")
+
+end Solution
+
