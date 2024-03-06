843517da7b336c490cb8bae992e1c7cef1e2a78e
Marbeck <raphael@watchnext.fr>
2024-03-06 15:30:21 +0100

    Day 17 OK still slow

--- day17/src/main/scala/Dijkstra.scala
+++ day17/src/main/scala/Dijkstra.scala
@@ -2,44 +2,55 @@ import scala.annotation.tailrec
 import scala.collection.mutable
 
 enum Direction:
-  def opposite: Direction =
-    this match
-      case Left2Right => Right2Left
-      case Right2Left => Left2Right
-      case Up2Down => Down2Up
-      case Down2Up => Up2Down
-  case Left2Right, Right2Left, Up2Down, Down2Up
+  def opposite: Direction = Direction.fromOrdinal((ordinal + 2) % Direction.values.length)
+  case Left2Right, Up2Down, Right2Left, Down2Up
 
 export Direction.*
 
-trait Valued[A]:
-  def index1: Int
-  def index2: Int
-  def getValue: Int
-  def hashed: Int
-  def cannotBeat(other: A): Boolean
+type ArrayOfFour[T <: Valued[T]] = Array[Array[Array[Array[Data[T]]]]]
 
-case class Summit(row: Int, col: Int, value: Int, lastDir: Direction, lastDirCounter: Int, height: Int) extends Valued[Summit]:
-  override def index1: Int = row + (col * height)
-  override def index2: Int = lastDir.ordinal + (lastDirCounter * Direction.values.length)
-  override def getValue: Int = -(row+1)*(col+1)
+type Valuer = (Int, Int) => Int
+
+trait Valued[A]:
+  def indexes: (Int, Int, Int, Int)
+  def getSecondOrderingValue: Int
+  def hashed: Int
+  def correctedDistance(rawDistance: Long): Long
+  def inBetween(other: A): Int
+  def to(other: A)(using allElements: Valuer): Seq[Int]
+
+case class Summit(row: Int, col: Int, value: Int, lastDir: Direction, lastDirCounter: Int) extends Valued[Summit]:
+  override def indexes: (Int, Int, Int, Int) = (row, col, lastDir.ordinal, lastDirCounter)
+  override def getSecondOrderingValue: Int = -(row + 1)*(col + 1)
+
+  override def correctedDistance(rawDistance: Long): Long = rawDistance
 
   override lazy val hashed: Int = hashCode()
 
-  override def cannotBeat(other: Summit): Boolean =
-    other match
-      case Summit(otherRow, otherCol, _ , otherLastDir , otherLastDirCounter, _) if otherRow == this.row && otherCol == this.col && otherLastDir == this.lastDir => otherLastDirCounter <= this.lastDirCounter
-      case _ => (this.row + this.col + 8) < (other.row + other.col)
+  override def inBetween(other: Summit): Int = math.abs(other.row - this.row) + math.abs(other.col - this.col)
+
+  override def to(other: Summit)(using allElements: Valuer): Seq[Int] =
+    other.row == row match
+      case true =>
+        (col until other.col by (other.col - col).sign).map(allElements(row, _))
+      case false =>
+        (row until other.row by (other.row - row).sign).map(allElements(_, col))
+
 
 class GraphFromArray(val elements: Seq[Summit])(validNeighbours: Summit => Seq[Summit]) extends Graph[Summit]:
   override def getElements: Seq[Summit] = elements
-  override def weightBetween(first: Data[Summit], second: Data[Summit]): Long = second.getElement.value
-  override def getNeighboursOfIn(current: Data[Summit], potentialNeighbours: Map[Summit, Data[Summit]]): Seq[Data[Summit]] =
+  override def weightBetween(first: Data[Summit], second: Data[Summit])(using allElements: ArrayOfFour[Summit]): Long =
+    second.getElement.inBetween(first.getElement) match
+      case 1 => second.getElement.value
+      case value =>
+        given Valuer = allElements(_: Int)(_: Int)(0)(0).getElement.value
+        second.getElement.to(first.getElement).sum
+  override def getNeighboursOfIn(current: Data[Summit], potentialNeighbours: ArrayOfFour[Summit]): Seq[Data[Summit]] =
     val possibleNeighbours = validNeighbours.apply(current.getElement)
-    possibleNeighbours.flatMap(potentialNeighbours.get)
-  override def getNeighboursOfIn(current: Data[Summit], potentialNeighbours: Array[Array[Data[Summit]]]): Seq[Data[Summit]] =
-    val possibleNeighbours = validNeighbours.apply(current.getElement)
-    possibleNeighbours.map(currentSummit => potentialNeighbours(currentSummit.index1)(currentSummit.index2))
+    possibleNeighbours.map:
+      currentSummit =>
+        val indexes = currentSummit.indexes
+        potentialNeighbours(indexes(0))(indexes(1))(indexes(2))(indexes(3))
 
 
 trait Graph[T <: Valued[T]]:
@@ -47,9 +58,8 @@ trait Graph[T <: Valued[T]]:
   def getListToExploreInitialisedFromStart(startingFrom: T): Seq[Data[T]] =
     val data = getElements.filterNot(_ == startingFrom).map(Data[T](_))
     Data(startingFrom, 0) +: data
-  def weightBetween(first: Data[T], second: Data[T]): Long
-  def getNeighboursOfIn(current: Data[T], potentialNeighbours: Map[T, Data[T]]): Seq[Data[T]]
-  def getNeighboursOfIn(current: Data[T], potentialNeighbours: Array[Array[Data[T]]]): Seq[Data[T]]
+  def weightBetween(first: Data[T], second: Data[T])(using allElements: ArrayOfFour[T]): Long
+  def getNeighboursOfIn(current: Data[T], potentialNeighbours: ArrayOfFour[T]): Seq[Data[T]]
 
 class Data[T <: Valued[T]](element: T, private var currentDistance: Long = Long.MaxValue):
   var explored: Boolean = false
@@ -59,58 +69,61 @@ class Data[T <: Valued[T]](element: T, private var currentDistance: Long = Long.
   private var precedingElement: Option[Data[T]] = None
   def getPreceding: Data[T] = precedingElement.get
   def getCurrentDistance: Long = currentDistance
-  def getBestFirst: (Long, Int) = (currentDistance, element.getValue)
+  def getBestFirst: (Long, Int) = (currentDistance, element.getSecondOrderingValue)
   def updateDistanceAndPreceding(newDistance: Long, preceding: Data[T]): Unit =
     currentDistance = newDistance
     precedingElement = Some(preceding)
 
 
   def markAsSolution = isASolution = true
+  def markExplored(using allElements: ArrayOfFour[T]) =
+    explored = true
+    val indexes = this.getElement.indexes
+    val nbElements = allElements(indexes(0))(indexes(1))(indexes(2)).length
+    (indexes(3) + 1 until nbElements).foreach:
+      allElements(indexes(0))(indexes(1))(indexes(2))(_).explored = true
 
-  def cannotBeat(other: Data[T]): Boolean =
-    getCurrentDistance > other.getCurrentDistance match
-      case true => this.element cannotBeat other.getElement
-      case false => false
 
 
   override def toString: String = s"{$element, $getCurrentDistance}"
 
 object Dijkstra:
-  def solve[T <: Valued[T]](graph: Graph[T], startingFrom: T, elementsToReach: List[T]): List[T] =
+  def solveOptimized[T <: Valued[T]](graph: Graph[T], startingFrom: T, elementsToReach: List[T], optimize: Boolean): List[T] =
     val toExplore = graph.getListToExploreInitialisedFromStart(startingFrom).toList
-    val mapOfSummits = toExplore.map(summitData => summitData.getElement -> summitData).toMap
-    doSolve[T](toExplore.toVector.take(1), Nil, elementsToReach, mapOfSummits)(graph)
-
-
-  def solveOptimized[T <: Valued[T]](graph: Graph[T], startingFrom: T, elementsToReach: List[T]): List[T] =
-    val toExplore = graph.getListToExploreInitialisedFromStart(startingFrom).toList
-    val heightOnIndex1 = toExplore.map(_.getElement.index1).max
-    val heightOnIndex2 = toExplore.map(_.getElement.index2).max
-    val allElements = Array.ofDim[Data[T]](heightOnIndex1 + 1, heightOnIndex2 + 1)
+    val widths = toExplore.map(_.getElement.indexes).max
+    val allElements = Array.ofDim[Data[T]](widths(0) + 1, widths(1) + 1, widths(2) + 1, widths(3) + 1)
     toExplore.foreach:
-      current => allElements(current.getElement.index1)(current.getElement.index2) = current
+      current =>
+        val indexes = current.getElement.indexes
+        allElements(indexes(0))(indexes(1))(indexes(2))(indexes(3)) = current
 
-    elementsToReach.map(element => allElements(element.index1)(element.index2).markAsSolution)
+    elementsToReach.map:
+      element =>
+        val indexes = element.indexes
+        allElements(indexes(0))(indexes(1))(indexes(2))(indexes(3)).markAsSolution
 
     given Ordering[Data[T]] with
       def compare(first: Data[T], second: Data[T]) =
-        first.getCurrentDistance.compare(second.getCurrentDistance) match
-          case 0 => first.getElement.getValue.compare(second.getElement.getValue) match
+        first.getElement.correctedDistance(first.getCurrentDistance).compare(second.getElement.correctedDistance(second.getCurrentDistance)) match
+          case 0 => first.getElement.getSecondOrderingValue.compare(second.getElement.getSecondOrderingValue) match
             case 0 => first.hashed.compare(second.hashed)
             case value => value
           case value => value
 
-    doSolveOptimized[T](mutable.TreeSet(toExplore.head), None, allElements)(graph)
+    doSolveOptimized[T](mutable.TreeSet(toExplore.head), None, optimize)(using graph)(using allElements)
 
 
   @tailrec
-  private def doSolveOptimized[T <: Valued[T]](toExplore: mutable.TreeSet[Data[T]], lastExplored: Option[Data[T]], allElements: Array[Array[Data[T]]])(implicit graph: Graph[T]): List[T] =
+  private def doSolveOptimized[T <: Valued[T]](toExplore: mutable.TreeSet[Data[T]], lastExplored: Option[Data[T]], optimize: Boolean)(using graph: Graph[T])(using allElements: ArrayOfFour[T]): List[T] =
     toExplore match
       case value if value.isEmpty => rewind(lastExplored.get)
       case _ if lastExplored.fold(false)(_.isASolution) => rewind(lastExplored.get)
       case value =>
-        val best = toExplore.head
-        val tail = toExplore.tail
+        val toExploreFiltered = optimize match
+          case true => toExplore.span(_.explored)._2
+          case false => toExplore
+        val best = toExploreFiltered.head
+        val tail = toExploreFiltered.tail
         val neighboursUpdated = graph.getNeighboursOfIn(best, allElements).flatMap: neighbour =>
           val distance = best.getCurrentDistance + graph.weightBetween(best, neighbour)
           if (neighbour.getCurrentDistance > distance)
@@ -122,31 +135,10 @@ object Dijkstra:
 
         tail.addAll(neighboursUpdated)
 
-        doSolveOptimized(tail, Some(best), allElements)
+        if (optimize)
+          best.markExplored
 
-
-  @tailrec
-  private def doSolve[T <: Valued[T]](toExplore: Vector[Data[T]], explored: List[Data[T]], elementsToReach: List[T], neighboursMap : Map[T, Data[T]])(implicit graph: Graph[T]): List[T] =
-    toExplore match
-      case value if value.isEmpty => rewind(explored.head)
-      case _ if  (explored.map(_.getElement) intersect elementsToReach).nonEmpty => println(explored.length); rewind(explored.head)
-      case value =>
-        val best = toExplore.head
-        val tail = toExplore.tail
-        //println(best)
-        val neighboursUpdated = graph.getNeighboursOfIn(best, neighboursMap).toVector.flatMap: neighbour =>
-          val distance = best.getCurrentDistance + graph.weightBetween(best, neighbour)
-          if (neighbour.getCurrentDistance > distance)
-            neighbour.updateDistanceAndPreceding(distance, best)
-            Some(neighbour)
-          else
-            None
-
-        val potentialNextListToExplore = (neighboursUpdated concat tail).sortBy(_.getBestFirst)
-        val futureBest = potentialNextListToExplore.head
-        val filtered = potentialNextListToExplore.filterNot(_ cannotBeat futureBest)
-
-        doSolve( filtered, best +: explored, elementsToReach, neighboursMap)
+        doSolveOptimized(tail, Some(best), optimize)
 
   @tailrec
   private def rewind[T <: Valued[T]](current: Data[T], previous: List[T] = Nil): List[T] =
--- day17/src/main/scala/Solution.scala
+++ day17/src/main/scala/Solution.scala
@@ -1,52 +1,54 @@
-val maxStepsInOneDirection = 3
+val maxStepsInOneDirectionPart1 = 3
+val minStepsInOneDirectionPart1 = 0
 val maxStepsInOneDirectionPart2 = 10
 val minStepsInOneDirectionPart2 = 4
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
     val (height: Int, width: Int) = (inputLines.length, inputLines(0).length)
-    val resultPart1 = {
-      given heatMap: HeatMap = HeatMap(inputLines)
 
-      val summits = heatMap.getSummits
+    given heatMap: HeatMap = HeatMap(inputLines)
 
-      val graphPart1 = GraphFromArray(summits)(nextPart1)
-      val startPart1 = summits.find:
-        case Summit(0, 0, _, _, 0, _) => true
-        case _ => false
-      .get
+    val summitsPart1 = heatMap.getSummitsPart1
 
-      val endPart1 = summits.toList.filter:
-        case Summit(row, col, _, Up2Down | Left2Right, _, _) if row == height - 1 && col == width - 1 => true
-        case _ => false
+    val graphPart1 = GraphFromArray(summitsPart1)(nextPart1)
+    val startPart1 = summitsPart1.find:
+      case Summit(0, 0, _, _, 0) => true
+      case _ => false
+    .get
 
-      Dijkstra.solveOptimized(graphPart1, startPart1, endPart1).drop(1).map(_.value).sum
-    }
+    val endPart1 = summitsPart1.toList.filter:
+      case Summit(row, col, _, Up2Down | Left2Right, _) if row == height - 1 && col == width - 1 => true
+      case _ => false
 
-    val resultPart2 = {
-      given heatMapPart2: HeatMap = HeatMap(inputLines)
+    val before = System.currentTimeMillis()
+    val resultPart1 = Dijkstra.solveOptimized(graphPart1, startPart1, endPart1, true).drop(1).map(_.value).sum
 
-      val summitsPart2 = heatMapPart2.getSummitsPart2
-      val startPart2 = summitsPart2.find:
-        case Summit(0, 0, _, _, 0, _) => true
-        case _ => false
-      .get
+    println(s"Done in ${System.currentTimeMillis() - before} ms")
 
-      val endPart2 = summitsPart2.toList.filter:
-        case Summit(row, col, _, Up2Down | Left2Right, lastDirCounter, _) if row == height - 1 && col == width - 1 && lastDirCounter >= minStepsInOneDirectionPart2 => true
-        case _ => false
+    val summitsPart2 = heatMap.getSummitsPart2
+    val startPart2 = summitsPart2.find:
+      case Summit(0, 0, _, _, 0) => true
+      case _ => false
+    .get
 
-      val graphPart2 = GraphFromArray(summitsPart2)(nextPart2)
-      val solutions = Dijkstra.solveOptimized(graphPart2, startPart2, endPart2)
-      solutions.drop(1).map(_.value).sum
-    }
+    val endPart2 = summitsPart2.toList.filter:
+      case Summit(row, col, _, Up2Down | Left2Right, lastDirCounter) if row == height - 1 && col == width - 1 && lastDirCounter >= minStepsInOneDirectionPart2 => true
+      case _ => false
+
+    val graphPart2 = GraphFromArray(summitsPart2)(nextPart2)
+    val solutions = Dijkstra.solveOptimized(graphPart2, startPart2, endPart2, false)
+
+    val resultPart2 = solutions.drop(1).map(_.value).sum
 
     val result1 = s"$resultPart1"
     val result2 = s"$resultPart2"
 
     (s"${result1}", s"${result2}")
 
-  def nextPart1(summit: Summit)(using heatMap: HeatMap): List[Summit] =
+
+  private def next(summit: Summit, min: Int, max: Int)(using heatMap: HeatMap): List[Summit] =
     val (currentRow, currentCol) = (summit.row, summit.col)
+
     Direction.values.toList.filterNot(_ == summit.lastDir.opposite).flatMap:
       currentDir =>
         val (nextRow, nextCol) = currentDir match
@@ -59,64 +61,46 @@ object Solution:
           case (false, _) => None
           case (true, true) =>
             summit.lastDirCounter match
-              case value if value == maxStepsInOneDirection => None
-              case value => Some(summit.copy(row = nextRow, col = nextCol, value = heatMap.getHeat(nextRow, nextCol), lastDirCounter = value + 1))
-          case (true, false) => Some(summit.copy(row = nextRow, col = nextCol, value = heatMap.getHeat(nextRow, nextCol), lastDir = currentDir, lastDirCounter = 1))
-
-  def nextPart2(summit: Summit)(using heatMap: HeatMap): List[Summit] =
-    val (currentRow, currentCol) = (summit.row, summit.col)
-    Direction.values.toList.filterNot(_ == summit.lastDir.opposite).flatMap:
-      currentDir =>
-        val (nextRow, nextCol) = currentDir match
-          case Left2Right => (currentRow, currentCol + 1)
-          case Right2Left => (currentRow, currentCol - 1)
-          case Up2Down => (currentRow + 1, currentCol)
-          case Down2Up => (currentRow - 1, currentCol)
-
-        (heatMap.isDefinedAt(nextRow, nextCol), summit.lastDir == currentDir) match
-          case (false, _) => None
-          case (true, true) =>
-            summit.lastDirCounter match
-              case value if value == maxStepsInOneDirectionPart2 => None
+              case value if value == max => None
               case value => Some(summit.copy(row = nextRow, col = nextCol, value = heatMap.getHeat(nextRow, nextCol), lastDirCounter = value + 1))
           case (true, false) =>
             summit.lastDirCounter match
-              case value if value < minStepsInOneDirectionPart2 => None
+              case value if value < min => None
               case value => Some(summit.copy(row = nextRow, col = nextCol, value = heatMap.getHeat(nextRow, nextCol), lastDir = currentDir, lastDirCounter = 1))
+
+  private def nextPart1(summit: Summit)(using heatMap: HeatMap): List[Summit] = next(summit, minStepsInOneDirectionPart1, maxStepsInOneDirectionPart1)
+
+  private def nextPart2(summit: Summit)(using heatMap: HeatMap): List[Summit] = next(summit, minStepsInOneDirectionPart2, maxStepsInOneDirectionPart2)
 
 end Solution
 
 case class HeatMap(inputLines: Seq[String]):
-  val (height: Int, width: Int) = (inputLines.length, inputLines(0).length)
+  private val (height: Int, width: Int) = (inputLines.length, inputLines(0).length)
   val data = inputLines.map(_.toCharArray.map(_.asDigit)).toArray
 
-  lazy val getSummits: Seq[Summit] =
+  private def getSummits(summitsGetter: (Int, Int, Int) => List[Summit]): Seq[Summit] =
     (for
       row <- 0 until height
       col <- 0 until width
     yield
-      allSummitsFrom(row, col, data(row)(col))
-    ).flatten
-
-  lazy val getSummitsPart2: Seq[Summit] =
-    (for
-      row <- 0 until height
-      col <- 0 until width
-    yield
-      allSummitsFromPart2(row, col, data(row)(col))
+      summitsGetter.apply(row, col, data(row)(col))
       ).flatten
+
+  lazy val getSummitsPart1: Seq[Summit] = getSummits(allSummitsFromPart1)
+
+  lazy val getSummitsPart2: Seq[Summit] = getSummits(allSummitsFromPart2)
 
   def isDefinedAt(row: Int, col: Int): Boolean = (row >= 0 && row < height) && (col >= 0 && col < width)
   def getHeat(row: Int, col: Int): Int = data(row)(col)
 
-  def allSummitsFrom(row: Int, col: Int, value: Int): List[Summit] =
+  private def allSummitsFromPart1(row: Int, col: Int, value: Int): List[Summit] =
     Direction.values.toList.flatMap:
       currentDir =>
-        (0 to maxStepsInOneDirection).map:
-          step => Summit(row, col, value, currentDir, step, height)
+        (0 to maxStepsInOneDirectionPart1).map:
+          step => Summit(row, col, value, currentDir, step)
 
-  def allSummitsFromPart2(row: Int, col: Int, value: Int): List[Summit] =
+  private def allSummitsFromPart2(row: Int, col: Int, value: Int): List[Summit] =
     Direction.values.toList.flatMap:
       currentDir =>
         (0 to maxStepsInOneDirectionPart2).map:
-          step => Summit(row, col, value, currentDir, step, height)
+          step => Summit(row, col, value, currentDir, step)
