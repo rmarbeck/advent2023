47e28c51cd9e929e27e14f5ba1801867ec6ae8d4
rmarbeck <rafoufou>
2024-03-11 10:56:31 +0100

    Day 21 Really faster

--- day21/src/main/scala/Solution.scala
+++ day21/src/main/scala/Solution.scala
@@ -6,12 +6,10 @@ object Solution:
   def run(inputLines: Seq[String]): (String, String) =
     given garden: Garden = Garden.from(inputLines)
 
-    given IGarden = IGarden(garden)
-
     val resultPart1 = count(List(garden.startingPosition), 64)
 
+    given AdvancedIGarden = AdvancedIGarden(garden)
     val steps: Long = 26_501_365
-
     val resultPart2 = Suite.calc(steps)
 
     val result1 = s"$resultPart1"
@@ -52,8 +50,8 @@ case class Suite(u0: Long, u1: Long, factor: Long):
   private def calc(n:Long): Long = n*u1 - (n - 1) *u0 + (n*(n-1)/2)*factor
 
 object Suite:
-  def calc(steps: Long)(using IGarden): Long =
-    val sizeOfInitialGarden = summon[IGarden].garden.height
+  def calc(steps: Long)(using AdvancedIGarden): Long =
+    val sizeOfInitialGarden = summon[AdvancedIGarden].garden.height
     val halfSize = sizeOfInitialGarden / 2
     val stepsToWalk: Long = (steps - halfSize) / sizeOfInitialGarden
     val firstRound = sizeOfInitialGarden + halfSize
@@ -61,8 +59,8 @@ object Suite:
 
     guessTerms(List(halfSize, firstRound, secondRound)).calc(stepsToWalk)
 
-  def guessTerms(terms: List[Int])(using IGarden): Suite =
-    val List(u2, u1, u0) = summon[IGarden].guessTerms(terms)
+  def guessTerms(terms: List[Int])(using AdvancedIGarden): Suite =
+    val List(u2, u1, u0) = summon[AdvancedIGarden].guessTerms(terms)
     val factor = u2 - 2*u1 + u0
     Suite(u0, u1, factor)
 
@@ -85,9 +83,8 @@ object Garden:
         line => line.toCharArray.map(Place.from)
     Garden(places)
 
-case class IGarden(garden: Garden):
-  import scala.collection.mutable.Map
-  private val cache: Map[(Int, Int), List[Position]] = Map()
+
+case class AdvancedIGarden(garden: Garden):
   private lazy val startingPosition: Position = garden.startingPosition
   private def slide(position: Position): Position =
     def signedModulo(value: Int, modulo: Int) =
@@ -96,23 +93,30 @@ case class IGarden(garden: Garden):
         case current => (value % modulo)
     Position(signedModulo(position.row, garden.height), signedModulo(position.col, garden.width))
 
-  private def nextInfinite(position: Position): List[Position] =
-    cacheActive match
-      case true => cache.getOrElseUpdate((position.row, position.col), List(position.north, position.south, position.east, position.west).filterNot(isARock))
-      case false => List(position.north, position.south, position.east, position.west).filterNot(isARock)
-
-
   def guessTerms(terms: List[Int]): List[Long] =
+    import scala.collection.mutable.Map
+    val visited: Map[Position, Boolean] = Map(this.startingPosition -> true)
+    def alreadyVisited(position: Position): Boolean =
+      visited.contains(position) match
+        case false =>
+          visited.put(position, true)
+          false
+        case true => true
+    def nextFrontierOnly(position: Position): Set[Position] =
+      Set(position.north, position.south, position.east, position.west).filterNot(isARock).filterNot(alreadyVisited)
+
     @tailrec
-    def count(positions: Set[Position], toFind: List[Int], results: List[Long] = Nil, counter: Int = 1): List[Long] =
+    def countAdvanced(frontier: Set[Position], previous: Int, beforePrevious: Int, toFind: List[Int], results: List[Long] = Nil, counter: Int = 0): List[Long] =
       toFind match
         case Nil => results
         case head :: tail =>
-          val newPositions = positions.flatMap(nextInfinite)
+          val nextFrontier = frontier.flatMap(nextFrontierOnly)
+          val nextFrontierSize = nextFrontier.size
           counter match
-            case value if value == head => count(newPositions, tail, newPositions.size +: results, counter + 1)
-            case _ => count(newPositions, toFind, results, counter + 1)
+            case value if value == head => countAdvanced(nextFrontier, beforePrevious + frontier.size, previous, tail, (beforePrevious + frontier.size) +: results, counter + 1)
+            case _ => countAdvanced(nextFrontier, beforePrevious + frontier.size, previous, toFind, results, counter + 1)
 
-    count(Set(this.startingPosition), terms)
+
+    countAdvanced(Set(this.startingPosition), 0, 0, terms)
 
   private def isARock(position: Position): Boolean = garden.isARock(slide(position))
