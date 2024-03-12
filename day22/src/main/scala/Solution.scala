a0cdb67b9f0912953d285644a60ffb796fc3d18f
rmarbeck <rafoufou>
2024-03-12 15:10:22 +0100

    Day 22 OK

--- day22/src/main/scala/Solution.scala
+++ day22/src/main/scala/Solution.scala
@@ -5,38 +5,25 @@ import collection.parallel.CollectionConverters.seqIsParallelizable
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
-    val bricks = inputLines.map(Brick.from)
+    val flyingBricks = inputLines.map(FlyingBrick.from)
 
-    val container = Container()
-    bricks.sortBy(_.lowestCube).foreach(container.dropBrick)
+    val brickContainer = BrickContainer.fromUnSortedFlyingBricks(flyingBricks)
 
-    //container.bricks.foreach(println)
-    println("Ready")
-
-    val resultPart1 = container.bricks.par.count:
-      current =>
-        val others = container.bricks.filterNot(_ == current)
-        val bricksSupported = current.listSupported(others)
-        val canBeDisintegrated = bricksSupported match
+    val (canBeDisintegrated, cannotBeDisintegrated) = brickContainer.staticBricks.par.partition:
+        _.listSupported match
           case Nil => true
-          case supportedList =>
-            supportedList.forall:
-              currentSupported =>
-                val othersWithoutCurrent = others.filterNot(_ == currentSupported)
-                currentSupported.countSupporting(othersWithoutCurrent) >= 1
+          case supportedList => supportedList.forall(_.listSupporting.length > 1)
 
-        canBeDisintegrated
+    val resultPart2 = cannotBeDisintegrated.par.map(_.computeHowManyFalls).sum
 
-    val result1 = s"$resultPart1"
-    val result2 = s""
+    val result1 = s"${canBeDisintegrated.length}"
+    val result2 = s"$resultPart2"
 
     (s"${result1}", s"${result2}")
 
 end Solution
 
-case class Coordinates(x: Int, y: Int, z: Int):
-  def asList = List(x, y , z)
-  def asTuple = (x, y , z)
+case class Coordinates(x: Int, y: Int, z: Int)
 
 case class Cube(coordinates: Coordinates):
   def goDown(numberOfSteps: Int): Cube = Cube(coordinates.copy(z = coordinates.z - numberOfSteps))
@@ -44,65 +31,89 @@ case class Cube(coordinates: Coordinates):
     others.exists:
       cube => cube.coordinates.x == this.coordinates.x && cube.coordinates.y == this.coordinates.y && cube.coordinates.z == this.coordinates.z + 1
 
-case class Brick(name: String, cubes: List[Cube]):
-  def overlap(other: Brick): Boolean = (other.cubes.map(_.coordinates) intersect cubes.map(_.coordinates)).nonEmpty
-  def support(other: Brick): Boolean = cubes.exists(_.isRightBelowOneOf(other.cubes))
-  def listSupported(others: List[Brick]): List[Brick] = others.filter(support)
-  def countSupported(others: List[Brick]): Int = others.count(support)
-  def isSupportedBy(other: Brick): Boolean = other.support(this)
-  def countSupporting(others: List[Brick]): Int = others.count(isSupportedBy)
-  def listSupporting(others: List[Brick]): List[Brick] = others.filter(isSupportedBy)
-  def goDown(numberOfSteps: Int): Brick = this.copy(cubes = cubes.map(_.goDown(numberOfSteps)))
+case class FlyingBrick(name: String, cubes: List[Cube]):
+  def goDown(numberOfSteps: Int): FlyingBrick = this.copy(cubes = cubes.map(_.goDown(numberOfSteps)))
 
   lazy val lowestCube = cubes.map(_.coordinates.z).min
 
   lazy val highsAndLows: Map[(Int, Int), (Int, Int)] =
     val altitudes = cubes.groupMap(current => (current.coordinates.x, current.coordinates.y))(_.coordinates.z)
-    altitudes.map(current => current._1 -> (current._2.max, current._2.min))
+    altitudes.map:
+      case ((x, y), zList) => (x, y) -> (zList.max, zList.min)
 
-  lazy val highs: List[(Int, Int, Int)] = highsAndLows.map(current => (current._1._1, current._1._2, current._2._1)).toList
-  lazy val lows: List[(Int, Int, Int)] = highsAndLows.map(current => (current._1._1, current._1._2, current._2._2)).toList
+  lazy val lows: List[(Int, Int, Int)] = highsAndLows.toList.map:
+    case ((x, y), (_, minimum)) => (x, y, minimum)
 
   override def toString: String = s"Brick : $name [$lowestCube]"
 
-object Brick:
-  def from(inputString: String): Brick =
+object FlyingBrick:
+  def from(inputString: String): FlyingBrick =
     val cubes = inputString match
       case s"$xs,$ys,$zs~$xe,$ye,$ze" =>
-        val List(xStart, yStart, zStart, xEnd, yEnd, zEnd) = List(xs, ys, zs, xe, ye, ze).map(_.toInt)
-        val couples = List((xStart, xEnd), (yStart, yEnd), (zStart, zEnd))
-        val List(xRange, yRange, zRange): List[Int] = couples.map(couple => (couple._1 - couple._2).abs)
-        val List(xMin, yMin, zMin): List[Int] = couples.map(couple => math.min(couple._1, couple._2))
+        val (starts, ends) = List(xs, ys, zs, xe, ye, ze).map(_.toInt).splitAt(3)
+        val couples = starts.zip(ends)
+        val List(xMin, yMin, zMin): List[Int] = couples.map(math.min(_, _))
+        val List(xMax, yMax, zMax): List[Int] = couples.map(math.max(_, _))
         for
-          x <- xMin to xMin + xRange
-          y <- yMin to yMin + yRange
-          z <- zMin to zMin + zRange
+          x <- xMin to xMax
+          y <- yMin to yMax
+          z <- zMin to zMax
         yield
           Cube(Coordinates(x, y, z))
       case _ => throw Exception("Not supported")
-    Brick(inputString, cubes.toList)
+    FlyingBrick(inputString, cubes.toList)
 
-class Container(var bricks: List[Brick] = List()):
-  import scala.collection.mutable.Map
-  val highestLevels: Map[(Int, Int), Int] = Map()
-  def addBrickInFinalPosition(brick: Brick): Unit =
-    brick.highs.foreach:
-      (x, y, z) => highestLevels.get((x, y)) match
-        case Some(currentValue) if currentValue < z => highestLevels.update((x, y), z)
-        case None => highestLevels.update((x, y), z)
-        case _ => ()
-    bricks = brick +: bricks
+case class StaticBrick(flyingBrick: FlyingBrick, brickContainer: BrickContainer):
+  lazy val highs: Map[(Int, Int), Int] = flyingBrick.highsAndLows.map:
+    case ((x, y), (maximum, _)) => (x, y) -> maximum
 
-  def brickIsNotTouchingAny(brick: Brick): Boolean =
-    brick.lows.forall:
-      (x, y, z) => highestLevels.get((x, y)) match
-        case Some(currentValue) if currentValue < z => true
-        case None => true
-        case _ => false
+  private def support(other: StaticBrick): Boolean = flyingBrick.cubes.exists(_.isRightBelowOneOf(other.flyingBrick.cubes))
+  lazy val listSupported: List[StaticBrick] =
+    brickContainer.staticBricks.filterNot(_ == this).filter(support)
 
-  @tailrec
-  final def dropBrick(brick: Brick): Unit =
-    val movedDown = brick.goDown(1)
-    movedDown.lowestCube > 1 && brickIsNotTouchingAny(movedDown) match
-      case true => dropBrick(movedDown)
-      case false => addBrickInFinalPosition(brick)
+  lazy val listSupporting: List[StaticBrick] =
+    def isSupportedBy(other: StaticBrick): Boolean = other.support(this)
+    brickContainer.staticBricks.filterNot(_ == this).filter(isSupportedBy)
+
+  lazy val computeHowManyFalls: Int =
+    @tailrec
+    def compute(currentBricks: List[StaticBrick], toRemove: List[StaticBrick] = Nil, currentCount: Int = 0): Int =
+      currentBricks match
+        case Nil => currentCount
+        case head :: tail =>
+          val falling = head.listSupported.filter:
+            _.listSupporting.filterNot(toRemove.contains).length <= 1
+          compute(tail ::: falling, head +: toRemove, currentCount + falling.length)
+
+    compute(List(this))
+
+case class BrickContainer(sortedFlyingBricks: List[FlyingBrick]):
+  val staticBricks: List[StaticBrick] =
+    sortedFlyingBricks.foldLeft(BuildingContainer()):
+      (acc, newFlyingBrick) => acc.add(newFlyingBrick, this)
+  .getBricks
+
+  override def toString: String = s"${staticBricks.map(_.flyingBrick).foreach(println)}"
+
+object BrickContainer:
+  def fromUnSortedFlyingBricks(unSortedFlyingBricks: Seq[FlyingBrick]): BrickContainer =
+    BrickContainer(unSortedFlyingBricks.sortBy(_.lowestCube).toList)
+
+case class BuildingContainer(staticBricks: List[StaticBrick] = Nil, highestLevels: Map[(Int, Int), Int] = Map()):
+  def getBricks = staticBricks
+  def add(flyingBrick: FlyingBrick, brickContainer: BrickContainer): BuildingContainer =
+    def newHighestLevelsWith(staticBrick: StaticBrick): Map[(Int, Int), Int] =
+      val currentBricksHighs = staticBrick.highs
+      (highestLevels.keySet ++ currentBricksHighs.keySet).map:
+        (x, y) => (x, y) -> math.max(highestLevels.getOrElse((x, y), 1), currentBricksHighs.getOrElse((x, y), 0) + 1)
+      .toMap
+    def dropBrick: StaticBrick =
+      val toDrop = flyingBrick.lows.map:
+        (x, y, z) => highestLevels.get((x, y)) match
+          case Some(currentValue) => z - currentValue
+          case None => z - 1
+      .min
+      StaticBrick(flyingBrick.goDown(toDrop), brickContainer)
+    val newStaticBrick = dropBrick
+    val newHighLevels = newHighestLevelsWith(newStaticBrick)
+    BuildingContainer(newStaticBrick +: staticBricks, newHighLevels)
--- day22/src/test/scala/MySuite.scala
+++ day22/src/test/scala/MySuite.scala
@@ -4,9 +4,9 @@ class MySuite extends munit.FunSuite:
   test("Day22 : main test on final data"):
     val (score1, score2) = Solver.solve
     assertEquals(score1, "454")
-    assertEquals(score2, "")
+    assertEquals(score2, "74287")
 
   test("Day22 : main test on initial data"):
     val (score1, score2) = Solver.solveTest
     assertEquals(score1, "5")
-    assertEquals(score2, "")
+    assertEquals(score2, "7")
