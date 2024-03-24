2929f16a81c9f97d2fe91166effe0e9624f7ce00
Marbeck <raphael@watchnext.fr>
2024-03-24 18:28:56 +0100

    Day 4 using regexp

--- day4/src/main/scala/Solution.scala
+++ day4/src/main/scala/Solution.scala
@@ -1,21 +1,22 @@
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
-    val cards = inputLines.map(Card.fromString)
+
+    val cards = inputLines.collect:
+      case CardExt(card) => card
 
     val resultPart1 = cards.map(_.score).sum
 
     val totalCards = Array.fill(cards.size)(1)
     for
-      currentCard <- cards
+      (currentCard, index) <- cards.zipWithIndex
       winnings = currentCard.winnings
       if winnings != 0
-      currentCardIndex = currentCard.id - 1
     do
-      val numberOfCurrentCard = totalCards(currentCardIndex)
-      (currentCardIndex + 1 to currentCardIndex + winnings).foreach:
-        case index =>
-          totalCards.isDefinedAt(index) match
-            case true => totalCards(index) += numberOfCurrentCard
+      val numberOfCurrentCard = totalCards(index)
+      (index + 1 to index + winnings).foreach:
+        case innerIndex =>
+          totalCards.isDefinedAt(innerIndex) match
+            case true => totalCards(innerIndex) += numberOfCurrentCard
             case false => ()
 
     val resultPart2 = totalCards.sum
@@ -27,17 +28,31 @@ object Solution:
 
 end Solution
 
-case class Card(id: Int, winning: List[Int], draw: List[Int]):
+case class Card(winning: List[Int], draw: List[Int]):
   lazy val winnings: Int = (draw intersect winning).size
   lazy val score: Int =
     winnings match
       case 0 => 0
       case value => math.pow(2, value - 1).toInt
 
-object Card:
-  def fromString(inputString: String): Card =
-    def extractNumbers(partialString: String): List[Int] =
-      partialString.split(" ").filterNot(_.isEmpty).map(_.toInt).toList
-    inputString match
-      case s"Card $id: $winnings | $draw" => Card(id.trim.toInt, extractNumbers(winnings), extractNumbers(draw))
-      case _ => throw Exception("Not supported")
+
+
+object CardExt:
+
+  val CardMatcher = """Card[ ]+(\d+): (.+) \| (.+)""".r
+  def unapply(str: String): Option[Card] =
+    str match
+      case CardMatcher(cardId, NumbersExt(winnings), NumbersExt(draws)) => Some(Card(winnings, draws))
+      case _ => None
+
+  object NumbersExt:
+    val NumbersMatcher = """(\d+)""".r
+    def unapply(str: String): Option[List[Int]] =
+      val values =
+        for
+          case NumbersMatcher(value) <- NumbersMatcher.findAllIn(str).toList
+        yield
+          value.toInt
+      values.size match
+        case 0 => None
+        case _ => Some(values)
