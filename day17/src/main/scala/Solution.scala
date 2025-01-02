c76aead9fad60e1d5a138eae1de85198c4cd9af2
rmarbeck <rafoufou>
2025-01-02 09:01:17 +0100

    Day 17 (cleaned)

--- day17/src/main/scala/Solution.scala
+++ day17/src/main/scala/Solution.scala
@@ -1,13 +1,10 @@
 import scala.annotation.{tailrec, targetName}
 import scala.collection.immutable.TreeSet
-import scala.collection.parallel.immutable.ParVector
-import scala.concurrent.{Await, Future}
-import scala.concurrent.ExecutionContext.Implicits.global
 
-val maxStepsInOneDirectionPart1 = 3
-val minStepsInOneDirectionPart1 = 1
-val maxStepsInOneDirectionPart2 = 10
-val minStepsInOneDirectionPart2 = 4
+val maxStepsInOneDirPart1 = 3
+val minStepsInOneDirPart1 = 1
+val maxStepsInOneDirPart2 = 10
+val minStepsInOneDirPart2 = 4
 
 type HeatMapper = Position => Long
 type Dimension = (Int, Int)
@@ -20,13 +17,7 @@ object Solution:
 
     given Dimension = (width, height)
 
-    given HeatMapper =
-      (for
-        y <- heats.indices; x <- heats.head.indices
-      yield
-        Position(x, y) -> heats(y)(x).toLong
-        ).toMap.apply
-
+    given HeatMapper = (position: Position) => heats(position.y)(position.x)
 
     import scala.collection.parallel._
     import collection.parallel.CollectionConverters.ImmutableSeqIsParallelizable
@@ -39,8 +30,6 @@ object Solution:
 
     (s"$result1", s"$result2")
 
-end Solution
-
 def startingCrucible(using CrucibleConstraints, Dimension): Seq[(Long, Crucible)] =
   summon[CrucibleConstraints] match
     case CrucibleConstraints.Part1 => Seq((0L, Crucible.init(Left2Right)))
@@ -49,8 +38,8 @@ def startingCrucible(using CrucibleConstraints, Dimension): Seq[(Long, Crucible)
 case class CrucibleConstraints(maxStepsInOneDirection: Int, minStepsInOneDirection: Int)
 
 object CrucibleConstraints:
-  val Part1: CrucibleConstraints = CrucibleConstraints(maxStepsInOneDirectionPart1, minStepsInOneDirectionPart1)
-  val Part2: CrucibleConstraints = CrucibleConstraints(maxStepsInOneDirectionPart2, minStepsInOneDirectionPart2)
+  val Part1: CrucibleConstraints = CrucibleConstraints(maxStepsInOneDirPart1, minStepsInOneDirPart1)
+  val Part2: CrucibleConstraints = CrucibleConstraints(maxStepsInOneDirPart2, minStepsInOneDirPart2)
 
 case class Position(x: Int, y: Int)(using dimension: Dimension):
   private inline def exists: Option[Position] = Option.when(x >= 0 && x < dimension._1 && y >=0 && y < dimension._2)(this)
@@ -63,26 +52,27 @@ case class Position(x: Int, y: Int)(using dimension: Dimension):
       case Down2Up => this.copy(y = y - 1)
       ).exists
 
-case class Crucible(position: Position, lastDir: Direction, lastDirCounter: Int)(using dimension: Dimension, crucibleConstraints: CrucibleConstraints):
+case class Crucible(position: Position, lastDir: Direction, lastDirCounter: Int)(using dimension: Dimension, constraints: CrucibleConstraints):
   private def nextStep(newDir: Direction): Option[Crucible] =
-    def dirCounter: Int = if (newDir == lastDir) then lastDirCounter + 1 else 1
+    def dirCounter: Int = if newDir == lastDir then lastDirCounter + 1 else 1
     (position + newDir).map(newPos => Crucible(newPos, newDir, dirCounter))
 
-  lazy val turn90CW: Option[Crucible] = nextStep(lastDir.turnCW)
-  lazy val turn90CCW: Option[Crucible] = nextStep(lastDir.turnCCW)
-  lazy val forward: Option[Crucible] = nextStep(lastDir)
+  private lazy val turn90CW: Option[Crucible] = nextStep(lastDir.turnCW)
+  private lazy val turn90CCW: Option[Crucible] = nextStep(lastDir.turnCCW)
+  private lazy val forward: Option[Crucible] = nextStep(lastDir)
 
   lazy val andMore: Set[Crucible] =
-    if (lastDirCounter >= crucibleConstraints.minStepsInOneDirection)
-      (lastDirCounter to crucibleConstraints.maxStepsInOneDirection).map(value => this.copy(lastDirCounter = value)).toSet
+    if (lastDirCounter >= constraints.minStepsInOneDirection)
+      (lastDirCounter to constraints.maxStepsInOneDirection).map(value => this.copy(lastDirCounter = value)).toSet
     else
       Set(this)
 
   def next: Seq[Crucible] =
-    lastDirCounter match
-      case value if value < crucibleConstraints.minStepsInOneDirection => Seq(forward).flatten
-      case value if value < crucibleConstraints.maxStepsInOneDirection => Seq(turn90CCW, turn90CW, forward).flatten
-      case _ => Seq(turn90CCW, turn90CW).flatten
+    (lastDirCounter match
+      case value if value < constraints.minStepsInOneDirection => Seq(forward)
+      case value if value < constraints.maxStepsInOneDirection => Seq(turn90CCW, turn90CW, forward)
+      case _ => Seq(turn90CCW, turn90CW)
+      ).flatten
 
 object Crucible:
   given ordering: Ordering[Crucible] = Ordering.by(nav => (-nav.position.x, -nav.position.y, nav.lastDirCounter, nav.lastDir.ordinal))
@@ -93,20 +83,20 @@ def heatLoss(toExplore: TreeSet[(Long, Crucible)], forbidden: Set[Crucible], goa
   toExplore match
     case empty if empty.isEmpty => 0
     case notEmpty =>
-      val (head, tail) = (toExplore.head, toExplore.tail)
-      if (forbidden.contains(head._2))
+      val ((currentDistance, crucible), tail) = (toExplore.head, toExplore.tail)
+      if (forbidden.contains(crucible))
         heatLoss(tail, forbidden, goal)
       else
-        val currentHeatLoss = head._1
-        if (head._2.position == goal && head._2.lastDirCounter >= crucibleConstraints.minStepsInOneDirection)
+        val currentHeatLoss = currentDistance
+        if (crucible.position == goal && crucible.lastDirCounter >= crucibleConstraints.minStepsInOneDirection)
           currentHeatLoss
         else
-          heatLoss(tail ++ head._2.next.map(newPosition => (currentHeatLoss + heatMapper.apply(newPosition.position), newPosition)), forbidden ++ head._2.andMore, goal)
+          heatLoss(tail ++ crucible.next.map(newPosition => (currentHeatLoss + heatMapper.apply(newPosition.position), newPosition)), forbidden ++ crucible.andMore, goal)
 
 enum Direction:
   case Left2Right, Up2Down, Right2Left, Down2Up
 
-  def turnCW: Direction = Direction.fromOrdinal((ordinal + 1) % Direction.values.length)
-  def turnCCW: Direction = Direction.fromOrdinal((ordinal + 3) % Direction.values.length)
+  def turnCW: Direction = Direction.fromOrdinal((ordinal + 1) % 4)
+  def turnCCW: Direction = Direction.fromOrdinal((ordinal + 3) % 4)
 
-export Direction.*
+export Direction._
