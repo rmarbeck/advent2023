80c0cea39b2112cf16df87b573e278639077bef4
rmarbeck <rafoufou>
2025-01-22 21:09:10 +0100

    going forward

--- day23b/src/main/scala/Solution.scala
+++ day23b/src/main/scala/Solution.scala
@@ -1,32 +1,55 @@
 import scala.annotation.tailrec
-import scala.collection.immutable.TreeSet
+import scala.collection.immutable.{BitSet, TreeSet}
+import scala.collection.mutable
+import scala.collection.mutable.Map
+
+type Dimension = Int
+type Target = (Int, Int)
 
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
     val result1 =
-      given Grid = Grid.fromInputPart1(inputLines)
-      val dimension = summon[Grid].size
-      solver(TreeSet((0, Summit(1, 0), Nil)), Set(), Summit(dimension-2, dimension-1)).get.toString
-
+      given Context = Context.fromInputPart1(inputLines)
+      solver(TreeSet(Path.from(Summit(1, 0))), Summit.target).get.toString
 
     val result2 =
-      given Grid = Grid.fromInputPart2(inputLines)
-
-      val dimension = summon[Grid].size
-      solver(TreeSet((0, Summit(1, 0), Nil)), Set(), Summit(dimension - 2, dimension - 1)).get.toString
+      given Context = Context.fromInputPart2  (inputLines)
+      solver(TreeSet(Path.from(Summit(1, 0))), Summit.target).get.toString
 
 
     (s"$result1", s"$result2")
 
+case class Context(grid: Grid, dimension: Dimension, target: Target)
+
+object Context:
+  private def from(arrayOfVals: Array[Array[Char]]): Context =
+    val grid = new Grid(arrayOfVals)
+    val dimension = grid.size
+    new Context(grid, dimension, (dimension - 2, dimension - 1))
+
+  private def slopeAsNormalPath(char: Char): Char =
+    char match
+      case '#' => '#'
+      case _ => '.'
+
+  def fromInputPart1(inputLines: Seq[String]): Context = from(inputLines.map(_.toCharArray).toArray.transpose)
+  def fromInputPart2(inputLines: Seq[String]): Context = from(inputLines.map(_.toCharArray.map(slopeAsNormalPath)).toArray.transpose)
+
+
+
 class Grid(private val arrayOfVals: Array[Array[Char]]):
-  lazy val size = arrayOfVals.length
-  def next(summit: Summit): Seq[Summit] =
+  lazy val size: Int = arrayOfVals.length
+  val cachedNext : mutable.Map[Summit, Seq[Summit]] = mutable.Map.empty
+
+  def nextCached(summit: Summit)(using context:Context): Seq[Summit] =
+    cachedNext.getOrElseUpdate(summit, next(summit))
+
+  def next(summit: Summit)(using context:Context): Seq[Summit] =
     val Summit(x, y) = summit
     def nextValid(deltas: Seq[(Int, Int)]): Seq[Summit] =
       deltas.map((dX, dY) => (x+dX, y+dY))
-        .filter((newX, newY) => arrayOfVals.isDefinedAt(newX) && arrayOfVals(newX).isDefinedAt(newY))
-        .filter((newX, newY) => arrayOfVals(newX)(newY) != '#')
+        .withFilter((newX, newY) => arrayOfVals.isDefinedAt(newX) && arrayOfVals(newX).isDefinedAt(newY) && arrayOfVals(newX)(newY) != '#')
         .map((newX, newY) => Summit(newX, newY))
 
     val atMost = arrayOfVals(x)(y) match
@@ -40,32 +63,48 @@ class Grid(private val arrayOfVals: Array[Array[Char]]):
 
     nextValid(atMost)
 
-object Grid:
-  private def slopeAsNormalPath(char: Char): Char =
-    char match
-      case '#' => '#'
-      case _ => '.'
-
-  def fromInputPart1(inputLines: Seq[String]): Grid = new Grid(inputLines.map(_.toCharArray).toArray.transpose)
-  def fromInputPart2(inputLines: Seq[String]): Grid = new Grid(inputLines.map(_.toCharArray.map(slopeAsNormalPath)).toArray.transpose)
-
-case class Summit(x: Int, y: Int):
-  def next(using grid: Grid): Seq[Summit] = grid.next(this)
+case class Summit(x: Int, y: Int)(using context:Context):
+  lazy val asInt: Int = (x + 1)*context.dimension + (y + 1)
+  private val (targetX, targetY) = context.target
+  lazy val distanceToTarget: Int = (targetX - x).abs + (targetY - y).abs
+  def next: Seq[Summit] = context.grid.next(this)
 
 object Summit:
-  given orderingSummit: Ordering[Summit] = Ordering.by(s => (s.x, s.y))
-  given orderingSummits: Ordering[List[Summit]] = Ordering.by(_.headOption)
+  def target(using context:Context): Summit = Summit(context.target._1, context.target._2)
 
-extension (self: List[Summit])
-  def containsTwice(summit: Summit) = self.count(_ == summit) >= 2
+case class Path(distance: Int, currentSummit: Summit, summits: BitSet)(using context: Context):
+  def next: Seq[Path] =
+    context.grid.nextCached(currentSummit).withFilter:
+      case summit: Summit if summits.contains(summit.asInt) => false
+      case _ => true
+    .map(summit => Path(distance+1, summit, summits + summit.asInt))
+
+
+object Path:
+  given ordering: Ordering[Path] =
+    given Ordering[BitSet] = BitsetOrdering.updateBitSet
+    Ordering.by(p => (-p._2.distanceToTarget, -p.distance,  p.summits))
+  def from(summit: Summit)(using context:Context): Path = new Path(0, summit, BitSet(summit.asInt))
+
+object BitsetOrdering:
+  given updateBitSet: Ordering[BitSet] with
+    override def compare(x: BitSet, y: BitSet): Int =
+      x == y match
+        case true => 0
+        case false =>
+          x diff y match
+            case difference if difference.isEmpty => -1
+            case _=> 1
 
 @tailrec
-def solver(toExplore: TreeSet[(Int, Summit, List[Summit])], explored: Set[Summit], toReach: Summit)(using grid: Grid): Option[Int] =
+def solver(toExplore: TreeSet[Path], toReach: Summit, found: Option[Int] = None)(using context:Context): Option[Int] =
   toExplore match
     case empty if empty.isEmpty => None
     case notEmpty =>
       notEmpty.head match
-        case (distance, summit, list) if summit == toReach => Some(distance)
-        case (distance, summit, list) =>
-          val currentPath = toExplore.head._3
-          solver(toExplore.tail ++ summit.next.filterNot(currentPath.contains).map(sum => (distance - 1, sum, sum :: list)), explored + notEmpty.head._2, toReach)
+        case Path(distance, summit, _) if summit == toReach => Some(distance)
+        case head @ Path(distance, _, _) =>
+          /*if (head.currentSummit.distanceToTarget < 100)
+            println(s"${toExplore.size} => ${notEmpty.head.distance} (${notEmpty.head.currentSummit.distanceToTarget})")
+          //println(s"${notEmpty.size} => ${notEmpty.head.distance} (${notEmpty.head.currentSummit.distanceToTarget})")*/
+          solver(toExplore.tail ++ head.next, toReach, found)
--- day23b/src/test/scala/MySuite.scala
+++ day23b/src/test/scala/MySuite.scala
@@ -3,10 +3,10 @@
 class MySuite extends munit.FunSuite:
   test("Day23b : main test on final data"):
     val (score1, score2) = Solver.solve
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "2186")
+    assertEquals(score2, "0")
 
   test("Day23b : main test on initial data"):
     val (score1, score2) = Solver.solveTest
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "94")
+    assertEquals(score2, "0")
