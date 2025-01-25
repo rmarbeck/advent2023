40706278c0e37e705e5813745de74633cacd6fc2
rmarbeck <rafoufou>
2025-01-25 08:51:12 +0100

    before recursive resolution

--- day23b/src/main/scala/Solution.scala
+++ day23b/src/main/scala/Solution.scala
@@ -1,3 +1,4 @@
+import javax.print.attribute.standard.Destination
 import scala.annotation.tailrec
 import scala.collection.immutable.{BitSet, TreeSet, Map as iMap}
 import scala.collection.mutable
@@ -9,18 +10,18 @@ type Target = (Int, Int)
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
-    val result1 = 0
+    val result1 = 0/*
       given Context = Context.fromInputPart1(inputLines)
-      val edges = findVertices(List(VerticesPath.init), BitSet(), Set(Vertex(1, Summit(1, 0)), Vertex(0, Summit.target)), Set(), Set())._2
-      given graph: Graph = Graph(edges)
-      println(solverExp(TreeSet(NewPath(0, graph.start, BitSet())), graph.target.summit, None))
+      val edges = findVertices(List(VerticesPath.init), BitSet(), Set(Vertex(1, Summit(1, 0)), Vertex(0, Summit.target)), Set(), Set())._1
+      given graph: Graph[DirectedEdge] = Graph(edges)
+      println(solverExp(TreeSet(NewPath(0, graph.start, BitSet())), graph.target.summit, None))*/
 
 
     val result2 =
       given Context = Context.fromInputPart2(inputLines)
       val edges = findVertices(List(VerticesPath.init), BitSet(), Set(Vertex(1, Summit(1, 0)), Vertex(0, Summit.target)), Set(), Set())._2
       //edges.toList.sortBy(_.lowestId).foreach(println)
-      given graph: Graph = Graph(edges)
+      given graph: Graph[UndirectedEdge] = Graph(edges)
 
       println(solverExp(TreeSet(NewPath(0, graph.start, BitSet())), graph.target.summit, None))
 
@@ -32,15 +33,30 @@ type WeightUnit = Int
 
 case class Vertex(id: Id, summit: Summit)
 
-case class Edge(vertices: Set[Vertex], weight: WeightUnit):
-  lazy val lowestId: Id = math.min(head, last)
+trait Edge:
+  def vertices: Set[Vertex]
+  def weight: WeightUnit
+  def head: Id
+  def last: Id
+  def toSeqOfMap: Seq[(Id,Connections)]
+  def lowestId: Id = math.min(head, last)
+
+object Edge:
+  def undirectedEdge(vertices: Set[Vertex], weight: WeightUnit): UndirectedEdge = UndirectedEdge(vertices, weight)
+  def undirectedEdge(vertex1: Vertex, vertex2: Vertex, weight: WeightUnit): UndirectedEdge = UndirectedEdge(Set(vertex1, vertex2), weight)
+  def directedEdge(head: Vertex, last: Vertex, weight: WeightUnit): DirectedEdge = DirectedEdge(head, last, weight)
+
+case class DirectedEdge(headVertex: Vertex, lastVertex: Vertex, weight: WeightUnit) extends Edge:
+  lazy val head: Id = headVertex.id
+  lazy val last: Id = lastVertex.id
+  lazy val vertices: Set[Vertex] = Set(headVertex, lastVertex)
+  override def toSeqOfMap: Seq[(Id,Connections)] = Seq(head -> Connections.single(last, weight))
+
+case class UndirectedEdge(vertices: Set[Vertex], weight: WeightUnit) extends Edge:
   lazy val head : Id = vertices.head.id
   lazy val last : Id = vertices.last.id
 
-  def toSeqOfMap: Seq[(Id,Seq[(Id, WeightUnit)])] = Seq(head -> Seq(last -> weight), last -> Seq(head -> weight))
-
-object Edge:
-  def apply(vertex1: Vertex, vertex2: Vertex, weight: WeightUnit): Edge = new Edge(Set(vertex1, vertex2), weight)
+  override def toSeqOfMap: Seq[(Id,Connections)] = Seq(head -> Connections.single(last, weight), last -> Connections.single(head, weight))
 
 case class VerticesPath(distance: Int, currentSummit: Summit, lastVertex: Vertex)(using context: Context):
   lazy val summitId: Int = currentSummit.asInt
@@ -54,7 +70,7 @@ object VerticesPath:
   def init(using Context): VerticesPath = VerticesPath(-1, Summit(1, 0), Vertex(1, Summit(1, 0)))
 
 @tailrec
-def findVertices(toExplore: List[VerticesPath], explored: BitSet, vertices: Set[Vertex], edgesPart1: Set[Edge], edgesPart2: Set[Edge])(using context:Context): (Set[Edge], Set[Edge]) =
+def findVertices(toExplore: List[VerticesPath], explored: BitSet, vertices: Set[Vertex], edgesPart1: Set[DirectedEdge], edgesPart2: Set[UndirectedEdge])(using context:Context): (Set[DirectedEdge], Set[UndirectedEdge]) =
   toExplore match
     case Nil => (edgesPart1, edgesPart2)
     case head :: tail if explored.contains(head.summitId) => findVertices(tail, explored, vertices, edgesPart1, edgesPart2)
@@ -66,13 +82,13 @@ def findVertices(toExplore: List[VerticesPath], explored: BitSet, vertices: Set[
           val pathThroughVertex = next.filter(path => vertices.map(_.summit).contains(path.currentSummit)).filterNot(_.shortcut)
           val (newEdgesPart1, newEdgesPart2) = pathThroughVertex match
             case onlyOne :: Nil =>
-              (edgesPart1 + Edge(onlyOne.lastVertex, vertices.find(_.summit == onlyOne.currentSummit).get, onlyOne.distance + 1)
+              (edgesPart1 + Edge.directedEdge(onlyOne.lastVertex, vertices.find(_.summit == onlyOne.currentSummit).get, onlyOne.distance + 1)
                 ,
-                edgesPart2 + Edge(onlyOne.lastVertex, vertices.find(_.summit == onlyOne.currentSummit).get, onlyOne.distance + 1))
+                edgesPart2 + Edge.undirectedEdge(onlyOne.lastVertex, vertices.find(_.summit == onlyOne.currentSummit).get, onlyOne.distance + 1))
             case _ =>
               if (vertices.find(_.id == 0).map(_.summit).contains(head.currentSummit))
-                (edgesPart1 + Edge(head.lastVertex, vertices.find(_.summit == head.currentSummit).get, head.distance + 1),
-                  edgesPart2 + Edge(head.lastVertex, vertices.find(_.summit == head.currentSummit).get, head.distance + 1))
+                (edgesPart1 + Edge.directedEdge(head.lastVertex, vertices.find(_.summit == head.currentSummit).get, head.distance + 1),
+                  edgesPart2 + Edge.undirectedEdge(head.lastVertex, vertices.find(_.summit == head.currentSummit).get, head.distance + 1))
               else
                 (edgesPart1, edgesPart2)
 
@@ -80,49 +96,50 @@ def findVertices(toExplore: List[VerticesPath], explored: BitSet, vertices: Set[
         case onlyOne :: Nil => findVertices(onlyOne :: tail, explored + head.summitId, vertices, edgesPart1, edgesPart2)
         case several =>
           val newVertex = Vertex(vertices.size, head.currentSummit)
-          val newEdgesPart1 = edgesPart1 + Edge(head.lastVertex, newVertex, head.distance + 1)
-          val newEdgesPart2 = edgesPart2 + Edge(head.lastVertex, newVertex, head.distance + 1)
+          val newEdgesPart1 = edgesPart1 + Edge.directedEdge(head.lastVertex, newVertex, head.distance + 1)
+          val newEdgesPart2 = edgesPart2 + Edge.undirectedEdge(head.lastVertex, newVertex, head.distance + 1)
           //println(s"here : ${Edge(head.lastVertex, newVertex, head.distance + 1)}")
           findVertices(several.map(_.newVertex(newVertex)) ::: tail, explored + head.summitId, vertices + newVertex, newEdgesPart1, newEdgesPart2)
 
-class Graph(edges: Set[Edge]):
+
+case class Connections(destinations: BitSet, weights: iMap[Id, WeightUnit]):
+  def ++(other: Connections): Connections = Connections(destinations ++ other.destinations, weights ++ other.weights)
+
+object Connections:
+  def single(destination: Id, weight: WeightUnit): Connections = new Connections(BitSet(destination), iMap(destination -> weight))
+
+
+class Graph[T <: Edge](edges: Set[T]):
   val lookup: iMap[Id, Vertex] = edges.flatMap(_.vertices).map(vertex => vertex.id -> vertex).toMap
-  private val optimizedEdges: iMap[Id, Seq[(Id, WeightUnit)]] =
-    val test = edges.flatMap(_.toSeqOfMap)
-    val test2 = test.groupMapReduce(_._1)(_._2)(_ ++ _)
-    test2
+  private val optimizedEdges: iMap[Id, Connections] = edges.flatMap(_.toSeqOfMap).groupMapReduce(_._1)(_._2)(_ ++ _)
   val start: Vertex = lookup(1)
   val target: Vertex = lookup(0)
-  /*println("******")
-  println(lookup)
-  println(edges)
-  println(s"sum : ${edges.map(_.weight).sum}")
-  println(optimizedEdges(0).map((id, weight) => (lookup(id), weight)))
-  println("------")*/
-  def isConnectedToTarget(vertex: Vertex): Boolean = optimizedEdges(vertex.id).map(_._1).contains(0)
-  def connectedVertex(vertex: Vertex): Seq[(Vertex, WeightUnit)] = optimizedEdges(vertex.id).map((id, weight) => (lookup(id), weight))
+
+  def connectedVertex(vertex: Vertex): Connections = optimizedEdges(vertex.id)
 
 
-case class NewPath(distance: Int, currentVertex: Vertex, vertices: BitSet, path: String = "")(using graph: Graph):
-  lazy val connectedToTarget: Int = if graph.isConnectedToTarget(currentVertex) then 1 else 0
+case class NewPath(distance: Int, currentVertex: Vertex, vertices: BitSet, size: Int = 0)(using graph: Graph[_]):
   private lazy val distanceToTarget = currentVertex.summit.distanceToTarget
-  def next: Seq[NewPath] =
-    graph.connectedVertex(currentVertex).filterNot(nextVertex => vertices.contains(nextVertex._1.id)).map:
-      case (vertex, weight) => NewPath(distance + weight, vertex, vertices + currentVertex.id, path/*s"${currentVertex.summit} - (${distance + weight}), ${path}"*/)
+  def next: Set[NewPath] =
+    val Connections(possible, values) = graph.connectedVertex(currentVertex)
+    (possible diff vertices).map:
+      key =>
+        val weight = values(key)
+        val vertex = graph.lookup(key)
+        NewPath(distance + weight, vertex, vertices + currentVertex.id, size + 1)
 
 object NewPath:
   given ordering: Ordering[NewPath] =
     given Ordering[BitSet] = BitsetOrdering.updateBitSet
-    Ordering.by(p => (-p.distanceToTarget, -p.distance,  p.vertices))
+    Ordering.by(p => (-p.distanceToTarget, -p.distance, p.size))
 
 @tailrec
-def solverExp(toExplore: TreeSet[NewPath], toReach: Summit, found: Option[Int] = None)(using graph: Graph): Option[Int] =
+def solverExp(toExplore: TreeSet[NewPath], toReach: Summit, found: Option[Int] = None)(using graph: Graph[_]): Option[Int] =
   toExplore match
     case empty if empty.isEmpty => None
     case notEmpty =>
       notEmpty.head match
-        case NewPath(distance, summit, vertices, path) if summit.summit == toReach =>
-          //println(path)
+        case NewPath(distance, summit, summits,_) if summit.summit == toReach =>
           Some(distance)
         case head =>
           solverExp(toExplore.tail ++ head.next, toReach, found)
@@ -201,7 +218,7 @@ case class Path(distance: Int, currentSummit: Summit, summits: BitSet)(using con
 object Path:
   given ordering: Ordering[Path] =
     given Ordering[BitSet] = BitsetOrdering.updateBitSet
-    Ordering.by(p => (-p._2.distanceToTarget, -p.distance,  p.summits))
+    Ordering.by(p => (-p._2.distanceToTarget, -p.distance, p.summits))
   def from(summit: Summit)(using context:Context): Path = new Path(0, summit, BitSet(summit.asInt))
 
 object BitsetOrdering:
@@ -212,7 +229,7 @@ object BitsetOrdering:
         case false =>
           x diff y match
             case difference if difference.isEmpty => -1
-            case _=> 1
+            case _ => 1
 
 @tailrec
 def solver(toExplore: TreeSet[Path], toReach: Summit, found: Option[Int] = None)(using context:Context): Option[Int] =
