ab9539871e3c6bc7b4d957a1576de54b1420b4b9
rmarbeck <rafoufou>
2025-03-04 10:49:39 +0100

    [Day 18] Small refactoring

--- day18/src/main/scala/Solution.scala
+++ day18/src/main/scala/Solution.scala
@@ -1,23 +1,42 @@
+import RelativePosition.*
+import Direction.*
+
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
-    val startingEdge = List(Position(0, 0))
-
-    val edges = inputLines.foldLeft((startingEdge, startingEdge)):
-      case ((part1Acc, part2Acc), s"$direction $steps ($color)") =>
-        ((part1Acc.head nextBy(direction, steps)) +: part1Acc, (part2Acc.head nextBy color) +: part2Acc)
+    val edges = inputLines.foldLeft((Path.start, Path.start)):
+      case ((pathPart1, pathPart2), s"${DirectionExt(direction)} ${StepsExt(steps)} ($color)") =>
+        (pathPart1.moveForward(direction, steps), pathPart2.moveForward(color))
       case _ => throw Exception("Error in parsing")
 
-    val List(resultPart1, resultPart2) =
-      edges.toList.map(_.tail).map:
-        edgeList => Loop(edgeList).globalArea
+    val List(result1, result2) = edges.toList.map(Loop.from(_).globalArea)
 
-    val result1 = s"$resultPart1"
-    val result2 = s"$resultPart2"
+    (s"$result1", s"$result2")
 
-    (s"${result1}", s"${result2}")
+case class Path private (positions: List[Position]):
+  def moveForward(direction: Direction, steps: Int): Path =
+    Path((positions.head nextBy(direction, steps)) +: positions)
 
-end Solution
+  def moveForward(color: String): Path =
+    Path((positions.head nextBy color) +: positions)
+
+  def allButHead: List[Position] = positions.tail
+
+object DirectionExt:
+  def unapply(str: String): Option[Direction] =
+    if Direction.isDefinedFrom(str) then
+      Some(Direction.fromString(str))
+    else
+      None
+
+object StepsExt:
+  def unapply(str: String): Option[Int] = str.toIntOption
+
+object Path:
+  def start: Path = Path(List(Position(0, 0)))
+
+object Loop:
+  def from(path: Path): Loop = Loop(path.allButHead)
 
 case class Loop(points: List[Position]):
   lazy val internalArea: Long = area(0)
@@ -36,7 +55,7 @@ case class Loop(points: List[Position]):
 
   private def computeArea(edges: List[Position]): Long =
     val nextOnes = edges.tail :+ edges.head
-    val result = edges.zip(nextOnes).foldLeft(0l):
+    val result = edges.zip(nextOnes).foldLeft(0L):
       case (acc, bothPoints) =>
         val (x1, y1, x2, y2) = (bothPoints(0).row, bothPoints(0).col, bothPoints(1).row, bothPoints(1).col)
         acc + (x2 * y1) - (x1 * y2)
@@ -60,7 +79,7 @@ object Direction:
       case 1 => from(string.head).isDefined
       case _ => false
 
-  def from(char: Char): Option[Direction] =
+  private def from(char: Char): Option[Direction] =
     char match
       case 'U' | '3' => Some(Up)
       case 'R' | '0' => Some(Right)
@@ -73,15 +92,13 @@ object Direction:
       case true => from(string.head).get
       case false => throw Exception("Not supported")
 
-export Direction.*
-
 enum RelativePosition:
   case Above, Below, AtRight, AtLeft
-export RelativePosition.*
+
 
 case class Position(row: Long, col: Long):
-  lazy val south = this.copy(row = row + 1)
-  lazy val east = this.copy(col = col + 1)
+  lazy val south: Position = next(Down, 1)
+  lazy val east: Position = next(Right, 1)
 
   private def next(direction: Direction, steps: Long): Position =
     direction match
@@ -90,10 +107,7 @@ case class Position(row: Long, col: Long):
       case Left => this.copy(col = col - steps)
       case Right => this.copy(col = col + steps)
 
-  def nextBy(direction: String, steps: String): Position =
-    require(Direction.isDefinedFrom(direction))
-    require(steps.toIntOption.isDefined)
-    next(Direction.fromString(direction), steps.toInt)
+  def nextBy(direction: Direction, steps: Int): Position = next(direction, steps)
 
   def nextBy(rawInHexadecimal: String): Position =
     next.tupled(rawInHexadecimal.parsePart2)
@@ -111,7 +125,7 @@ extension (string: String)
     string match
       case s"#$hexadecimal" =>
         val (first, last) = hexadecimal.splitAt(5)
-        val steps = first.foldLeft(0l):
+        val steps = first.foldLeft(0L):
           (acc, char) => acc * 16 + char.asDigit
         (Direction.fromString(last), steps)
       case _ => throw Exception("Not in expected format")
